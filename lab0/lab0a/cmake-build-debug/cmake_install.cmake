e9cd9b6811cff168da0f49769741bb6f63ca2967
theql <theqlgames@gmail.com>
2023-09-10 15:28:37 +0700

    minor change

--- lab0/lab0a/cmake-build-debug/cmake_install.cmake
+++ lab0/lab0a/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/programming/projects/cpp-labs/lab0
+# Install script for directory: D:/programming/projects/cpp-labs/lab0/lab0a
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Project")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/lab0")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/programming/projects/cpp-labs/lab0/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/programming/projects/cpp-labs/lab0/lab0a/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
