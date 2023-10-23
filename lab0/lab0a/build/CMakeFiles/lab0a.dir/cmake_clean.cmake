5d1fe071fbe62d8250331e1b61485e86b8af056d
theqly <theqlgames@gmail.com>
2023-10-23 19:46:31 +0700

    ffff

---
+++ lab0/lab0a/build/CMakeFiles/lab0a.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab0a.dir/main.cpp.o"
+  "CMakeFiles/lab0a.dir/main.cpp.o.d"
+  "CMakeFiles/lab0a.dir/module1.cpp.o"
+  "CMakeFiles/lab0a.dir/module1.cpp.o.d"
+  "CMakeFiles/lab0a.dir/module2.cpp.o"
+  "CMakeFiles/lab0a.dir/module2.cpp.o.d"
+  "CMakeFiles/lab0a.dir/module3.cpp.o"
+  "CMakeFiles/lab0a.dir/module3.cpp.o.d"
+  "lab0a"
+  "lab0a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab0a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
