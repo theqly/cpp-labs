e4159c22ffd4262af7e9b96cc50b4e8efacd5d01
theqly <theqlgames@gmail.com>
2023-12-27 04:05:22 +0700

    add plant

--- lab2/src/plant.cpp
+++ lab2/src/plant.cpp
@@ -88,56 +88,6 @@ bool plant::load_texture(const std::string &path) {
 	sprite_clips[i].h = 64;
   }
 
-  sprite_clips[0].x = 0;
-  sprite_clips[0].y = 0;
-  sprite_clips[0].w = 64;
-  sprite_clips[0].h = 64;
-
-  sprite_clips[1].x = 64;
-  sprite_clips[1].y = 0;
-  sprite_clips[1].w = 64;
-  sprite_clips[1].h = 64;
-
-  sprite_clips[2].x = 128;
-  sprite_clips[2].y = 0;
-  sprite_clips[2].w = 64;
-  sprite_clips[2].h = 64;
-
-  sprite_clips[3].x = 192;
-  sprite_clips[3].y = 0;
-  sprite_clips[3].w = 64;
-  sprite_clips[3].h = 64;
-
-  sprite_clips[4].x = 256;
-  sprite_clips[4].y = 0;
-  sprite_clips[4].w = 64;
-  sprite_clips[4].h = 64;
-  //---------------
-  sprite_clips[5].x = 0;
-  sprite_clips[5].y = 64;
-  sprite_clips[5].w = 64;
-  sprite_clips[5].h = 64;
-
-  sprite_clips[6].x = 64;
-  sprite_clips[6].y = 64;
-  sprite_clips[6].w = 64;
-  sprite_clips[6].h = 64;
-
-  sprite_clips[7].x = 128;
-  sprite_clips[7].y = 64;
-  sprite_clips[7].w = 64;
-  sprite_clips[7].h = 64;
-
-  sprite_clips[8].x = 192;
-  sprite_clips[8].y = 64;
-  sprite_clips[8].w = 64;
-  sprite_clips[8].h = 64;
-
-  sprite_clips[9].x = 256;
-  sprite_clips[9].y = 64;
-  sprite_clips[9].w = 64;
-  sprite_clips[9].h = 64;
-
   box.w = 64;
   box.h = 64;
 
