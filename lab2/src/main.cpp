a7717052a3e4074a160bbe054ae30d2ce8ea0506
theqly <theqlgames@gmail.com>
2024-01-20 20:14:14 +0700

    done with exceptions

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,7 +1,12 @@
 #include "game.h"
 
 int main(int argc, char **argv) {
-	game sprout_lands(1920, 1080);
-	sprout_lands.run();
+	game sprout_lands(1980, 1080);
+	try{
+		sprout_lands.run();
+	}
+	catch(const std::runtime_error& e){
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
